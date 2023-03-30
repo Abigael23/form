97234f9040f4fa0ff18acd0ca692f83e199b4594
Abigael Daramola <abigaeldaramola2016@gmail'com>
2023-03-30 21:30:28 +0100

    create file for css and js

---
+++ signup.js
@@ -0,0 +1,20 @@
+// document.getElementById("signup").style.display = "initial"
+    // document.getElementById("login").style.display = "initial"
+    document.getElementById("signupdiv").style.display = "block"
+    document.getElementById("logindiv").style.display = "none"
+    // document.getElementById("signupdiv").style.backgroundColor = "red"
+   
+    function signupbutton() {
+        // document.getElementById("signup").style.display = "inline"
+        // document.getElementById("login").style.display = "inline"
+        document.getElementById("signupdiv").style.display = "block"
+        document.getElementById("logindiv").style.display = "none"
+        document.getElementById("signup1").style.backgroundColor = "green"
+    }
+    function loginbutton() {
+        // document.getElementById("signup").style.display = "inline"
+        // document.getElementById("login").style.display = "inline"
+        document.getElementById("signupdiv").style.display = "none"
+        document.getElementById("logindiv").style.display = "block"
+        document.getElementById("login2").style.backgroundColor = "blue"
+    }
