5a2ab63315d685c6d9f2228c03ac712ec4d59779
Abigael Daramola <abigaeldaramola2016@gmail'com>
2023-05-03 20:46:38 +0100

    css, js and html modifications

--- signup.js
+++ signup.js
@@ -1,27 +1,50 @@
-// document.getElementById("signup").style.display = "initial"
-    // document.getElementById("login").style.display = "initial"
     document.getElementById("signupdiv").style.display = "block"
     document.getElementById("logindiv").style.display = "none"
-    // document.getElementById("signupdiv").style.backgroundColor = "red"
-   
+    
+    let username = document.getElementById("username")
+    let email = document.getElementById("email")
+    let phonenumber = document.getElementById("phonenumber")
+    let password = document.getElementById("password")
+    let confirmpassword = document.getElementById("confirmpassword")
+
     function signupbutton() {
-        // document.getElementById("signup").style.display = "inline"
-        // document.getElementById("login").style.display = "inline"
         document.getElementById("signupdiv").style.display = "block"
         document.getElementById("logindiv").style.display = "none"
         document.getElementById("signup1").style.backgroundColor = "green"
     }
     function loginbutton() {
-        // document.getElementById("signup").style.display = "inline"
-        // document.getElementById("login").style.display = "inline"
         document.getElementById("signupdiv").style.display = "none"
         document.getElementById("logindiv").style.display = "block"
         document.getElementById("login2").style.backgroundColor = "blue"
     }
-//     function load(event){
-// document.getElementById("load").innerHTML = <button>LOADING...</button>
-//     }
-
-//     addEventListener(event){
-//         document.getElementById("load").innerHTML = <button>LOADING...</button>
-//     }
+    function signUp(ev){
+        ev.preventDefault();
+        let user ={
+            username: username.value,
+            email: email.value,
+            phonenumber: phonenumber.value,
+            password: password.value,
+            confirmpassword: confirmpassword.value
+        }
+        console.log(user);
+        if (password.value.length < 6) {
+            alert("Password too weak")
+        }else if(password.value === confirmpassword.value){
+            alert("Sign Up successful")
+        } else if (password.value == "" || email.value == "" || phonenumber.value ==""|| username.value =="") {
+            alert("Input field cannot be empty")
+        } else if(password.value !== confirmpassword.value) {
+            alert("Password does not match")
+        }
+    }
+    function logIn(ev) {
+        ev.preventDefault();
+        let userdata ={
+            username: username1.value,
+            password: password1.value,
+            confirmpassword: confirmpassword1.value
+        }
+        console.log(userdata);
+        // localStorage.getItem("loggedUser", JSON.stringify(userdata))
+        // console.log(loggedUser);
+    }
